1f18c05d29dc80c0e1fed4e7acf9a4214e131759
skdhg <46562212+skdhg@users.noreply.github.com>
2024-04-17 00:36:31 +0545

    0.1.0

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'ohmyvm.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'vm.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.android-arm64.node')
+            nativeBinding = require('./vm.android-arm64.node')
           } else {
-            nativeBinding = require('ohmyvm-android-arm64')
+            nativeBinding = require('@ohmyvm/vm-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'ohmyvm.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'vm.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.android-arm-eabi.node')
+            nativeBinding = require('./vm.android-arm-eabi.node')
           } else {
-            nativeBinding = require('ohmyvm-android-arm-eabi')
+            nativeBinding = require('@ohmyvm/vm-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'ohmyvm.win32-x64-msvc.node')
+          join(__dirname, 'vm.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.win32-x64-msvc.node')
+            nativeBinding = require('./vm.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('ohmyvm-win32-x64-msvc')
+            nativeBinding = require('@ohmyvm/vm-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'ohmyvm.win32-ia32-msvc.node')
+          join(__dirname, 'vm.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.win32-ia32-msvc.node')
+            nativeBinding = require('./vm.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('ohmyvm-win32-ia32-msvc')
+            nativeBinding = require('@ohmyvm/vm-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'ohmyvm.win32-arm64-msvc.node')
+          join(__dirname, 'vm.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.win32-arm64-msvc.node')
+            nativeBinding = require('./vm.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('ohmyvm-win32-arm64-msvc')
+            nativeBinding = require('@ohmyvm/vm-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'ohmyvm.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'vm.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./ohmyvm.darwin-universal.node')
+        nativeBinding = require('./vm.darwin-universal.node')
       } else {
-        nativeBinding = require('ohmyvm-darwin-universal')
+        nativeBinding = require('@ohmyvm/vm-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'ohmyvm.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'vm.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.darwin-x64.node')
+            nativeBinding = require('./vm.darwin-x64.node')
           } else {
-            nativeBinding = require('ohmyvm-darwin-x64')
+            nativeBinding = require('@ohmyvm/vm-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'ohmyvm.darwin-arm64.node')
+          join(__dirname, 'vm.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.darwin-arm64.node')
+            nativeBinding = require('./vm.darwin-arm64.node')
           } else {
-            nativeBinding = require('ohmyvm-darwin-arm64')
+            nativeBinding = require('@ohmyvm/vm-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'ohmyvm.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'vm.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./ohmyvm.freebsd-x64.node')
+        nativeBinding = require('./vm.freebsd-x64.node')
       } else {
-        nativeBinding = require('ohmyvm-freebsd-x64')
+        nativeBinding = require('@ohmyvm/vm-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-x64-musl.node')
+            join(__dirname, 'vm.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-x64-musl.node')
+              nativeBinding = require('./vm.linux-x64-musl.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-x64-musl')
+              nativeBinding = require('@ohmyvm/vm-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-x64-gnu.node')
+            join(__dirname, 'vm.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-x64-gnu.node')
+              nativeBinding = require('./vm.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-x64-gnu')
+              nativeBinding = require('@ohmyvm/vm-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-arm64-musl.node')
+            join(__dirname, 'vm.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-arm64-musl.node')
+              nativeBinding = require('./vm.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-arm64-musl')
+              nativeBinding = require('@ohmyvm/vm-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-arm64-gnu.node')
+            join(__dirname, 'vm.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-arm64-gnu.node')
+              nativeBinding = require('./vm.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-arm64-gnu')
+              nativeBinding = require('@ohmyvm/vm-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -226,26 +226,26 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-arm-musleabihf.node')
+            join(__dirname, 'vm.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-arm-musleabihf.node')
+              nativeBinding = require('./vm.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-arm-musleabihf')
+              nativeBinding = require('@ohmyvm/vm-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-arm-gnueabihf.node')
+            join(__dirname, 'vm.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-arm-gnueabihf.node')
+              nativeBinding = require('./vm.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-arm-gnueabihf')
+              nativeBinding = require('@ohmyvm/vm-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -255,26 +255,26 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-riscv64-musl.node')
+            join(__dirname, 'vm.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-riscv64-musl.node')
+              nativeBinding = require('./vm.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-riscv64-musl')
+              nativeBinding = require('@ohmyvm/vm-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'ohmyvm.linux-riscv64-gnu.node')
+            join(__dirname, 'vm.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./ohmyvm.linux-riscv64-gnu.node')
+              nativeBinding = require('./vm.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('ohmyvm-linux-riscv64-gnu')
+              nativeBinding = require('@ohmyvm/vm-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -283,13 +283,13 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'ohmyvm.linux-s390x-gnu.node')
+          join(__dirname, 'vm.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./ohmyvm.linux-s390x-gnu.node')
+            nativeBinding = require('./vm.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('ohmyvm-linux-s390x-gnu')
+            nativeBinding = require('@ohmyvm/vm-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
